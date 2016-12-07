065d438b2ce06376d07469af75a701a301cb8782
Christopher Styles <christopherstyles@gmail.com>
2016-12-06 21:44:17 -0800

    Close idle connections before heroku timeout

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule ElectoralCollegeRollcall.UserSocket do
   # channel "room:*", ElectoralCollegeRollcall.RoomChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
