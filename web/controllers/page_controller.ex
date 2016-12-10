735c30c65a0afb38679c9193bfc78752edd4bd40
Christopher Styles <christopherstyles@gmail.com>
2016-12-10 03:17:22 -0800

    Group electors by state and reformat

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,16 +2,22 @@ defmodule ElectoralCollegeRollcall.PageController do
   use ElectoralCollegeRollcall.Web, :controller
 
   alias ElectoralCollegeRollcall.Elector
+  alias ElectoralCollegeRollcall.State
 
   plug :load_electors when action in [:index]
+  plug :load_states when action in [:index]
 
   def index(conn, _params) do
     render conn, "index.html"
   end
 
-  def load_electors(conn, _) do
+  defp load_electors(conn, _) do
     electors = Repo.all from e in Elector, preload: [:state]
-
     assign(conn, :electors, electors)
+  end
+
+  defp load_states(conn, _) do
+    states = Repo.all from s in State
+    assign(conn, :states, states)
   end
 end
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,13 @@
 defmodule ElectoralCollegeRollcall.PageView do
   use ElectoralCollegeRollcall.Web, :view
+
+  alias ElectoralCollegeRollcall.Repo
+  alias ElectoralCollegeRollcall.State
+
+  def grouped_electors(conn) do
+    Enum.group_by(conn.assigns[:electors], fn elector ->
+      Repo.get(State, elector.state_id).name
+    end)
+    |> Enum.sort
+  end
 end
